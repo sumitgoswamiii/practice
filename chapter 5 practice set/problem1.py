d89b8c3c56ed554869d944783b08c36c8f9528c9
Ritesh Goswami <riteshgoswami22201@gmail.com>
2026-02-07 20:39:42 +0530

    practice for list and tuples done

---
+++ chapter 5 practice set/problem1.py
@@ -0,0 +1,9 @@
+words = {
+    "madad":"help",
+    "kursi":"chair",
+    "billi":"cat",
+    "kutta":"dog",
+    "hathi":"elephant"
+}
+word = input("Enter the word you want meaning of: ")
+print(words[word])
---
+++ chapter 5 practice set/problem9.py
@@ -0,0 +1,4 @@
+s = {8, 7, 12, "harry", [1,2]}
+e = set()
+# list inside the set can't be updated
+print(s ,type(s))
